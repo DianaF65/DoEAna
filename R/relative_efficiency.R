4a79da5efe98d3282c4d69d4a8f7b9b5224105f9
rlscott <rebekah.lynne.scott@gmail.com>
2023-04-13 16:25:39 -0600

    Rel eff fix

--- R/relative_efficiency.R
+++ R/relative_efficiency.R
@@ -26,7 +26,7 @@ relative_efficiency <- function(m_row, e_row, c_row) {
   m_best <- parse_design_to_rmat(m_row)
   e_best <- parse_design_to_rmat(e_row)
   c_best <- parse_design_to_rmat(c_row)
-  all_candidates <- c(m_best, e_best, c_best)
+  all_candidates <- list(m_best, e_best, c_best)
 
   m_score <- m_row$score
   e_score <- e_row$score
@@ -38,14 +38,14 @@ relative_efficiency <- function(m_row, e_row, c_row) {
   for (i in seq_len(3)) {
     for (j in seq_len(3)) {
       eval_metrix <- metrics[i]
-      to_eval <- all_candidates[j]
-      top_eval <- min_Dist(to_eval, eval_metrix)
+      to_eval <- all_candidates[[j]]
+      top_eval <- min_dist(to_eval, eval_metrix[[1]])
       rel_eff <- (top_eval / all_scores[i]) * 100
-      all_eff[i, j] <- rel_eff
+      all_eff[i, j] <- round(rel_eff, 3)
     }
   }
   df <- data.frame(all_eff)
-  colnames(df) <- c(":M", ":E", ":C")
+  colnames(df) <- c("M Best", "E Best", "C Best")
   rownames(df) <- c("M(x)", "E(x)", "C(x)")
   return(df)
 }
