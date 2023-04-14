9d85409a5a59b4bb730faa017395d39881c417b0
DianaF65 <diana.felipe@usu.edu>
2023-04-14 10:09:16 -0600

    document

--- R/plot_design.R
+++ R/plot_design.R
@@ -1,3 +1,11 @@
+#' Plot Design Function
+#'
+#' This function plots hypercube and simplex geometries.
+#'
+#' @param data_row A row of data.
+#'
+#' @return A plot of the specified geometry.
+#' @export
  plot_design <- function(data_row) {
   # either selects hyper cube or simplex plot fxn
   if (data_row$geometry == "hypercube") {
@@ -5,7 +13,7 @@
   } else if (data_row$geometry == "simplex") {
     plot <- plot_simplex(data_row)
   } else {
-    warning("This either has to be a hyper cube or a simplex function.")
+    warning("This either has to be a hyper cube or a simplex.")
   }
    print(plot)
 }
@@ -18,17 +26,20 @@
 #' @param data_row A row of data.
 #'
 #' @return A plot of the hypercube.
-#' @export
 plot_hypercube <- function(data_row) {
   # ggplot2 example code
   design <- as.data.frame(parse_design_to_rmat(data_row))
   colnames(design) <- c("X1", "X2")
-  plot <- ggplot2::ggplot(design, ggplot2::aes(x = X1, y = X2)) + ggplot2::geom_point(size = 2, color = "blue") +
+  plot <- ggplot2::ggplot(design, ggplot2::aes(x = X1, y = X2)) +
+    ggplot2::geom_point(size = 2, color = "blue") +
     ggplot2::scale_y_continuous(limits = c(-1,1)) +
     ggplot2::scale_x_continuous(limits = c(-1,1)) +
-    ggplot2::labs(title = paste0(data_row$distance, " distance, n = ", data_row$n),
-         subtitle = paste0("min_dist score = ", round(data_row$score, 4)))+ ggplot2::theme(
-           plot.title = ggplot2::element_text(size = 14, face = "bold", hjust = 0.5),
+    ggplot2::labs(title = paste0(data_row$distance, " distance, n = ",
+                                 data_row$n),
+         subtitle = paste0("min_dist score = ", round(data_row$score, 4)))+
+    ggplot2::theme(
+           plot.title = ggplot2::element_text(size = 14, face = "bold",
+                                              hjust = 0.5),
            plot.subtitle = ggplot2::element_text(hjust = 0.5))
   return(plot)
 }
@@ -40,7 +51,6 @@ plot_hypercube <- function(data_row) {
 #' @param data_row A row of data.
 #'
 #' @return A plot of the simplex.
-#' @export
 plot_simplex <- function(data_row) {
   design <- as.data.frame(parse_design_to_rmat(data_row))
   colnames(design) <- c("X1", "X2", "X3")
@@ -51,14 +61,18 @@ plot_simplex <- function(data_row) {
                       yend = c(1, 1, 1)/3,
                       zend = c(1, 1, 1)/3)
 
-  suppressWarnings({plot <- ggtern::ggtern(design, ggplot2::aes(x = X1, y = X2, z = X3)) +
+  suppressWarnings({plot <- ggtern::ggtern(design, ggplot2::aes(x = X1,
+                                y = X2, z = X3)) +
     ggplot2::geom_point(size = 3, color = "blue") +
     ggplot2::geom_segment(data = lines, ggplot2::aes(x = x1, y = x2,  z = x3,
-                                                     xend = xend, yend = yend, zend = zend),
+                                xend = xend, yend = yend, zend = zend),
                           color = "grey", size = 0.2) +
-    ggplot2::labs(title = paste0(data_row$distance, " distance, n = ", data_row$n),
-                  subtitle = paste0("min_dist score = ", round(data_row$score, 4))) +
-    ggplot2::theme_bw() + ggtern::theme_nomask() + ggtern::theme_clockwise() + ggplot2::theme(
+    ggplot2::labs(title = paste0(data_row$distance, " distance, n = ",
+                                 data_row$n),
+                  subtitle = paste0("min_dist score = ",
+                                    round(data_row$score, 4))) +
+    ggplot2::theme_bw() + ggtern::theme_nomask() + ggtern::theme_clockwise() +
+    ggplot2::theme(
       plot.title = ggplot2::element_text(size = 14, face = "bold", hjust = 0.5),
       plot.subtitle = ggplot2::element_text(hjust = 0.5)) })
   return(plot)
